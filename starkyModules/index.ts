ba08fae3276ba24f5a31c67fe36fab228f3c4281
nmalzieu <noe.malzieu@gmail.com>
2022-09-13 14:11:12 +0200

    Remove the Argent detector for now

--- starkyModules/index.ts
+++ starkyModules/index.ts
@@ -1,9 +1,7 @@
-import * as argentWallet from "./argentWallet";
 import * as erc721 from "./erc721";
 import { StarkyModules } from "./types";
 
 const modules: StarkyModules = {
-  argentWallet,
   erc721,
 };
 
