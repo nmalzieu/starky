7acdecfe2747d002c2d453003b1d369a9f0a3944
Iris <iris.devillars@gmail.com>
2022-12-13 11:43:38 +0100

    run eslint & prettier

--- aspect/retrieveAssets.ts
+++ aspect/retrieveAssets.ts
@@ -1,23 +1,23 @@
 type RetrieveAssetsParameters = {
-    starknetNetwork: "mainnet" | "goerli";
-    contractAddress: string;
-    ownerAddress: string;
-  };
+  starknetNetwork: "mainnet" | "goerli";
+  contractAddress: string;
+  ownerAddress: string;
+};
 
 export const retrieveAssets = async ({
-    starknetNetwork,
-    contractAddress,
-    ownerAddress,
-  }: RetrieveAssetsParameters) => {
-    const network = starknetNetwork === "mainnet" ? "api" : "api-testnet";
-    const url = `https://${network}.aspect.co/api/v0/assets?contract_address=${contractAddress}&owner_address=${ownerAddress}`;
+  starknetNetwork,
+  contractAddress,
+  ownerAddress,
+}: RetrieveAssetsParameters) => {
+  const network = starknetNetwork === "mainnet" ? "api" : "api-testnet";
+  const url = `https://${network}.aspect.co/api/v0/assets?contract_address=${contractAddress}&owner_address=${ownerAddress}`;
 
-    const data = await fetch(url, {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    }).then((res) => res.json());
+  const data = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
 
-    return data.assets;
-  };
+  return data.assets;
+};
