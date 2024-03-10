8485a675e78181e7e1d5510ce65cf1da6bac7c85
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-03-10 17:07:14 +0100

    tweaking settings

--- discord/client.ts
+++ discord/client.ts
@@ -5,12 +5,14 @@ import config from "../config";
 
 export const createDiscordClient = (): Promise<Client> =>
   new Promise((resolve, reject) => {
+    console.log("> Connecting to Discord...");
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
     client.once("ready", () => {
       console.log("> Discord bot launched successfully");
       resolve(client);
     });
     client.login(config.DISCORD_BOT_TOKEN).catch((e) => {
+      console.error("Error occurred launching Discord bot", e);
       reject(e);
     });
   });
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -e
+fallocate -l 1024M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon /swapfile
 node dist-server/server.js
