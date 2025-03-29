194eb66262caf6bbe38465d90bf696627677b2ab
Nachiket <nachiket.roy.2@gmail.com>
2025-03-29 21:38:15 +0530

    replace a tag with Link and extra-links to extraLinks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 import Logo from "../components/Logo";
 import SocialLinks from "../components/SocialLinks";
@@ -16,9 +17,9 @@ const Home: NextPage = () => (
         >
           <h2>install our discord bot</h2>
         </a>
-      <div className={styles.extra-links}>
-          <a href="/help">▶ Starky: wtf?</a>
-          <a href="/examples">▶ Config examples</a>
+      <div className={styles.extraLinks}>
+          <Link href="/help">▶ Starky: wtf?</Link>
+          <Link href="/examples">▶ Config examples</Link>
        </div>
         <SocialLinks />
       </>
