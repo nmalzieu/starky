3eca741e88e0a1ff3a87b2596adced7c08017935
Nikhil rai <rainikhil044@gmail.com>
2025-04-10 14:24:34 +0530

    Fix: Add /dashboard/<guild-id>/<token> page with config list display

--- config.ts
+++ config.ts
@@ -6,8 +6,8 @@ dotenv.config({ path: ".env.local" });
 let DB_HOST = process.env.DB_HOST || "localhost";
 let DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432;
 let DB_USERNAME = process.env.DB_USERNAME || "postgres";
-let DB_PASSWORD = process.env.DB_PASSWORD || "mysecretpassword";
-let DB_DATABASE = process.env.DB_DATABASE || "postgres";
+let DB_PASSWORD = process.env.DB_PASSWORD || "HAALAND9";
+let DB_DATABASE = process.env.DB_DATABASE || "starky";
 let STARKSCAN_API_KEY = process.env.STARKSCAN_API_KEY || "";
 
 if (process.env.DATABASE_URL) {
@@ -21,10 +21,8 @@ if (process.env.DATABASE_URL) {
 
 const config = {
   NEXT_PUBLIC_DISCORD_CLIENT_ID:
-    process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "1359767529102377000",
-  DISCORD_BOT_TOKEN:
-    process.env.DISCORD_BOT_TOKEN ||
-    "MTM1OTc2NzUyOTEwMjM3NzAwMA.GWaXgu.SGoN2YTQlG-JnqemJeYZCqtgR46ujIZNnjpEm0",
+    process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID || "",
+  DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN || "",
   BASE_URL: process.env.BASE_URL || "http://localhost:8080",
   DB_HOST,
   DB_PORT,
