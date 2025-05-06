f9261cdb5fa0b325c92917f4b44b70c1b19bd13f
Faith Amarachi M. <oluebube.madu@yahoo.com>
2025-05-06 14:18:14 +0100

    Update index.ts

--- discord/index.ts
+++ discord/index.ts
@@ -1,100 +1,28 @@
-import { REST } from "@discordjs/rest";
-import { Client } from "discord.js";
+import config from "../config";
+import WatchTowerLogger from "../watchTower";
 
-import {
-  ButtonHandler,
-  ChatInputHandler,
-  interactionHandlers,
-  ModalSubmitHandler,
-  SelectMenuHandler,
-} from "../commands/index";
+import { createDiscordClient } from "./client";
+import { setupInteractions } from "./interactions";
+import { registerSlashCommands } from "./slashCommands";
 
-import config from "../../config";
-import WatchTowerLogger from "../../watchTower";
+const verifyConfig = () => {
+  if (!config.NEXT_PUBLIC_DISCORD_CLIENT_ID) {
+    throw new Error(`Missing config: NEXT_PUBLIC_DISCORD_CLIENT_ID`);
+  }
+  if (!config.DISCORD_BOT_TOKEN) {
+    throw new Error(`Missing config: DISCORD_BOT_TOKEN`);
+  }
+};
 
-export const restClient = new REST({ version: "10" }).setToken(
-  config.DISCORD_BOT_TOKEN
-);
-
-export const setupInteractions = (client: Client) => {
-  client.on("interactionCreate", async (interaction) => {
-    const matchingHandler = interactionHandlers.find((config) => {
-      switch (config.type) {
-        case "chatInput":
-          return (
-            interaction.isChatInputCommand() &&
-            interaction.commandName === config.identifier
-          );
-        case "button":
-          return (
-            interaction.isButton() &&
-            (config.matchType === "startsWith"
-              ? interaction.customId.startsWith(config.identifier)
-              : interaction.customId === config.identifier)
-          );
-        case "selectMenu":
-          return (
-            interaction.isStringSelectMenu() &&
-            interaction.customId === config.identifier
-          );
-        case "modalSubmit":
-          return (
-            interaction.isModalSubmit() &&
-            interaction.customId === config.identifier
-          );
-        default:
-          return false;
-      }
-    });
-
-    if (matchingHandler) {
-      switch (matchingHandler.type) {
-        case "chatInput":
-          if (interaction.isChatInputCommand()) {
-            return (matchingHandler.handler as ChatInputHandler)(
-              interaction,
-              client,
-              restClient
-            );
-          }
-          break;
-        case "button":
-          if (interaction.isButton()) {
-            return (matchingHandler.handler as ButtonHandler)(
-              interaction,
-              client,
-              restClient
-            );
-          }
-          break;
-        case "selectMenu":
-          if (interaction.isStringSelectMenu()) {
-            return (matchingHandler.handler as SelectMenuHandler)(
-              interaction,
-              client,
-              restClient
-            );
-          }
-          break;
-        case "modalSubmit":
-          if (interaction.isModalSubmit()) {
-            return (matchingHandler.handler as ModalSubmitHandler)(
-              interaction,
-              client,
-              restClient
-            );
-          }
-          break;
-      }
-    }
-
-    if (interaction.isRepliable()) {
-      await interaction.reply({
-        content: "This interaction couldn't be processed.",
-        ephemeral: true,
-      });
-    }
-  });
-
-  WatchTowerLogger.info("> Discord interactions set up successfully");
+export const launchBot = async () => {
+  try {
+    WatchTowerLogger.info("> Verifying configuration");
+    verifyConfig();
+  } catch (e: any) {
+    WatchTowerLogger.error(e.message, e);
+    return;
+  }
+  const client = await createDiscordClient();
+  await registerSlashCommands();
+  setupInteractions(client);
 };
