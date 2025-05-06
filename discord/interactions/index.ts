6a5453ccaaf5b421d7678dac8d2ae40a59ff238b
Faith Amarachi M. <oluebube.madu@yahoo.com>
2025-05-06 14:18:52 +0100

    Update index.ts

--- discord/interactions/index.ts
+++ discord/interactions/index.ts
@@ -12,11 +12,6 @@ import {
 import config from "../../config";
 import WatchTowerLogger from "../../watchTower";
 
-import {
-  handleInitialConfigCommand,
-  handleEditConfigButton, //  Importing the handleEditConfigButton function
-} from "../interactions/addConfigCommand";
-
 export const restClient = new REST({ version: "10" }).setToken(
   config.DISCORD_BOT_TOKEN
 );
@@ -90,21 +85,6 @@ export const setupInteractions = (client: Client) => {
             );
           }
           break;
-      }
-    }
-
-    // Handle specific interactions
-    if (
-      interaction.isChatInputCommand() &&
-      interaction.commandName === "starky-config"
-    ) {
-      return handleInitialConfigCommand(interaction, client, restClient); // Handles initial configuration
-    }
-
-    if (interaction.isButton()) {
-      if (interaction.customId === "starky-config-edit") {
-        //  Handle "Edit Configuration" button
-        return handleEditConfigButton(interaction, client, restClient); // Use the handleEditConfigButton function
       }
     }
 
